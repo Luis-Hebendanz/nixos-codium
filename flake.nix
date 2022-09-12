e08f27ce515326a9a1b26c3fd26e8091b603b85b
Luis-Hebendanz <luis.nixos@gmail.com>
2022-09-13 00:39:09 +0200

    Init repo

--- flake.nix
+++ flake.nix
@@ -1,8 +1,24 @@
 {
   description = "NixOS Codium in a shell";
 
-  outputs = { self, nixpkgs }: {
-    nixosModule = import ./modules/codium-nixos;
-    nixosModules.default = self.nixosModule;
-  };
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      rec {
+        packages = flake-utils.lib.flattenTree {
+          codium = pkgs.callPackage ./packages/codium-nixos {
+            extensionsFromVscodeMarketplace = pkgs.vscode-utils.extensionsFromVscodeMarketplace;
+            vscodeDefault = pkgs.vscodium;
+          };
+        };
+        defaultPackage = packages.codium;
+        apps.codium = flake-utils.lib.mkApp { drv = packages.codium; };
+        defaultApp = apps.codium;
+      }
+
+      #checks.${system}.integration-tests = 
+    );
 }
